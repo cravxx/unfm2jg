d50572801b4c636d202fa843e52591140bd7f2a2
Daffeinatek <samsquanchhunter14@gmail.com>
2016-01-11 19:34:28 -0600

    ok ok ok

--- update_docs.sh
+++ update_docs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo -e "Publishing javadoc...\n"
+echo "Publishing javadoc...\n"
 
 cp -R doc $HOME/javadoc-latest
 
@@ -10,10 +10,10 @@ git config --global user.name "travis-ci"
 git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/HulaSamsquanch/unfm2jg gh-pages > /dev/null
 
 cd gh-pages
-git rm -rf ./javadoc
-cp -Rf $HOME/javadoc-latest ./javadoc
+git rm -rf javadoc
+cp -Rf $HOME/javadoc-latest javadoc
 git add -f .
 git commit -m "Latest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
 git push -fq origin gh-pages > /dev/null
 
-echo -e "Published Javadoc to gh-pages.\n"
+echo "Published Javadoc to gh-pages.\n"
