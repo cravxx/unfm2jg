9ad9d93fdac16294500750a260c4e31e9e49f0ca
Daffeinatek <samsquanchhunter14@gmail.com>
2016-03-11 11:58:06 -0600

    new

--- update_docs.sh
+++ update_docs.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 
-echo "Publishing javadoc...\n"
 
-cp -R doc $HOME/javadoc-latest
+branch=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
+if ["$branch" = "master"] ;then
+    echo "Publishing javadoc...\n"
 
-cd $HOME
-git config --global user.email "travis@travis-ci.org"
-git config --global user.name "travis-ci"
-git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/HulaSamsquanch/unfm2jg gh-pages > /dev/null
+    cp -R doc $HOME/javadoc-latest
 
-cd gh-pages
-git rm -rf .
-cp -Rf $HOME/javadoc-latest .
-git add -f .
-git commit -m "Latest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
-git push -fq origin gh-pages > /dev/null
+    cd $HOME
+    git config --global user.email "travis@travis-ci.org"
+    git config --global user.name "travis-ci"
+    git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/HulaSamsquanch/unfm2jg gh-pages > /dev/null
 
-echo "Published Javadoc to gh-pages.\n"
+    cd gh-pages
+    git rm -rf .
+    cp -Rf $HOME/javadoc-latest .
+    git add -f .
+    git commit -m "Latest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
+    git push -fq origin gh-pages > /dev/null
+
+    echo "Published Javadoc to gh-pages.\n"
+fi else
+    echo "Not on master, not updating.\n"
+fi
+
+
+
