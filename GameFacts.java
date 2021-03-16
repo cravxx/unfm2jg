ba1cae3979b7f32d82477d84b40602071db6004c
cravxx <environskrakow@protonmail.com>
2021-03-16 08:55:28 -0600

    final

--- GameFacts.java
+++ GameFacts.java
@@ -6,15 +6,15 @@
  **/
 public class GameFacts
 {
-     public static int numberOfStages = 17; //make sure to change xtg loadmusic, xtg hipno, and xtg sndsize
+     public static final int numberOfStages = 17; //make sure to change xtg loadmusic, xtg hipno, and xtg sndsize
 
-     public static int numberOfCars = 16;
+     public static final int numberOfCars = 16;
 
      public static int numberOfPlayers = 7;
 
-     public static int screenWidth = 1280; //dont touch this if you don't know what you're doing
+     public static final int screenWidth = 1280; //dont touch this if you don't know what you're doing
 
-     public static int screenHeight = 720; //dont touch this if you don't know what you're doing
+     public static final int screenHeight = 720; //dont touch this if you don't know what you're doing
 
-     public static int polyLimit = 300; 
+     public static final int polyLimit = 300;
 }
